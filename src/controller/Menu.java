957aad27ca534f728ae30203079afaf746c73910
EvolutionProgramming <262878345+EvolutionProgramming@users.noreply.github.com>
2026-07-22 22:08:37 -0300

    feat: Desenvolvi e fiz o design do menu

--- src/controller/Menu.java
+++ src/controller/Menu.java
@@ -1,4 +1,19 @@
 package controller;
 
+import java.util.Scanner;
+
 public class Menu {
+    private Scanner sc = new Scanner(System.in);
+    int opcao;
+
+    public void exibirMenu() {
+        System.out.println("--------------------------------------------");
+        System.out.println("Digite 1 para ADICIONAR uma nova tarefa➕");
+        System.out.println("Digite 2 para MOSTRAR tarefas\uD83D\uDCCB");
+        System.out.println("Digite 3 para CONCLUIR tarefas✅");
+        System.out.println("Digite 4 para REMOVER tarefas❌");
+        System.out.println("Digite 0 para SAIR do sistema\uD83D\uDC4B");
+        System.out.println("--------------------------------------------");
+        opcao = sc.nextInt();
+    }
 }
